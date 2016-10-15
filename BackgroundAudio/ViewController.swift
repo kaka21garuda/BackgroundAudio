9df4e64e9fe46223504d9b76b318005f5ed1125e
Buka Cakrawala <kaka10garuda@gmail.com>
2016-10-14 20:44:55 -0700

    Audio is played

--- BackgroundAudio/ViewController.swift
+++ BackgroundAudio/ViewController.swift
@@ -7,19 +7,74 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+   var audioPlayer = AVAudioPlayer()
+    
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "sample", ofType: "mp3")!))
+            audioPlayer.prepareToPlay()
+            
+            var audioSession = AVAudioSession.sharedInstance()
+            
+            do {
+                try audioSession.setCategory(AVAudioSessionCategoryPlayback)
+                print("AVAudioSession Category Playback OK")
+                
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
 
+        } catch {
+            print(error)
+        }
+        
+        
+       
+        
+    
+        
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+    @IBAction func play(_ sender: AnyObject) {
+        audioPlayer.play()
+        
+        
+    }
+    
+    @IBAction func pause(_ sender: AnyObject) {
+        if audioPlayer.isPlaying {
+            audioPlayer.pause()
+        } else {
+            print("Audio is not playing!")
+        }
+    }
+
+
+    @IBAction func restart(_ sender: AnyObject) {
+        if audioPlayer.isPlaying {
+            audioPlayer.pause()
+        }
+        audioPlayer.currentTime = 0
+        audioPlayer.play()
+    }
+
 }
+
+
 
